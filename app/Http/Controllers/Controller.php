69f3f766d393edc63291c855918cdc7489c4455f
J4KUB-JS <jakubspirydonjs@gmail.com>
2024-05-25 12:46:33 +0200

    CRUD app

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,62 @@
 <?php
 
 namespace App\Http\Controllers;
+use Exception;
 
+use App\Models\Task;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        return ['tasks' => Task::all()];
+    }
+
+    public function show(string $id)
+    {
+        $task = Task::where('id', $id)->get();
+        $task->done = !$task->done;
+        $task->save();
+        return ['task' => $task ];
+    }
+
+    public function store(Request $request)
+    {   
+        try{
+            $newTask = new Task();
+            $newTask->name = $request->name;
+            $newTask->desc = $request->desc;
+            $newTask->dueDate = $request->dueDate;
+            $newTask->done = false;
+            $newTask->save();
+        } catch (Exception $e) {
+            return ['error' => 'smth went wrong'];
+        }
+
+        return ['task' => $newTask];
+    }
+
+    public function update(string $id, Request $request)
+    {
+        $task = Task::where('id', $id)->first();
+        $task->name = $request->name;
+        $task->desc = $request->desc;
+        $task->dueDate = $request->dueDate;
+        $task->done = $request->done;
+        $task->save();
+
+        return ['task' => $task];
+    }
+
+    public function destroy(string $id)
+    {
+        Task::where('id', $id)->delete();
+        return ['message' => "task with id: $id removed"];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('tasks', [Controller::class, 'index']);
+Route::post('tasks', [Controller::class, 'store']);
+Route::get('tasks/{id}', [Controller::class, 'show']);
+Route::put('tasks/{id}', [Controller::class, 'update']);
+Route::delete('tasks/{id}', [Controller::class, 'destroy']);
